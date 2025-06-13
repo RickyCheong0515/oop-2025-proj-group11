ab899a63a24e7b716a0d13063e86b572698ec831
RickyCheong0515 <101554818+RickyCheong0515@users.noreply.github.com>
2025-06-14 01:03:35 +0800

    Update modules_used.py

--- Data_crawling/modules_used.py
+++ Data_crawling/modules_used.py
@@ -2,3 +2,6 @@ import requests
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+!wget -O TaipeiSansTCBeta-Regular.ttf https://drive.google.com/uc?id=1eGAsTN1HBpJAkeVM57_C7ccp7hbgSz3_&export=download
+fontManager.addfont('TaipeiSansTCBeta-Regular.ttf')
+mpl.rc('font', family='Taipei Sans TC Beta')
