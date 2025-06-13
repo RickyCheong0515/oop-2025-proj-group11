78c9da3dea9fa6488201bf9c07a8de9250715124
RickyCheong0515 <101554818+RickyCheong0515@users.noreply.github.com>
2025-06-14 02:33:18 +0800

    Update Crawling.py

--- Data_crawling/Crawling.py
+++ Data_crawling/Crawling.py
@@ -11,17 +11,22 @@ for year in range(112,114,1):
         nameList = zf.namelist()
         for name in nameList:
             zf.extract(name, r'zipfolder')
+    #Get the Normal sales
     path = "/content/zipfolder/a_lvr_land_a.csv"
     df = pd.read_csv(path)
     df = df.drop(0)    #remove the first row(translation of each column)
     Non_Luxury_Sales = pd.concat([Non_Luxury_Sales,df])
     df = df.drop(columns=['主建物面積', '附屬建物面積', '陽台面積', '電梯', '移轉編號'])    #remove the no duplicates terms
     Sales_all = pd.concat([Sales_all,df])
-
+    #Get the Luxury sales
     path = "/content/zipfolder/a_lvr_land_b.csv"
     df = pd.read_csv(path)
     df = df.drop(0)    #remove the first row(translation of each column)
     Luxury_Sales = pd.concat([Luxury_Sales,df])
     df = df.drop(columns=['建案名稱', '棟及號', '解約情形'])    #remove the no duplicates terms
     Sales_all = pd.concat([Sales_all,df])
-    
+    #Get the Rent
+    path = "/content/zipfolder/a_lvr_land_c.csv"
+    df = pd.read_csv(path)
+    df = df.drop(0)    #remove the first row(translation of each column)
+    Rent_all = pd.concat([Rent_all,df])
