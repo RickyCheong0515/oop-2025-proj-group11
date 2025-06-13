ae785697d907a10993dac4becfc4feae5c97beb5
RickyCheong0515 <101554818+RickyCheong0515@users.noreply.github.com>
2025-06-14 00:52:10 +0800

    Update Sample_of_extract_zipfile.py

--- Data_crawling/Sample_of_extract_zipfile.py
+++ Data_crawling/Sample_of_extract_zipfile.py
@@ -1,5 +1,5 @@
+import requests
 import zipfile
-from zipfile import ZipFile
 url = "https://plvr.land.moi.gov.tw/DownloadSeason?season=114S1&type=zip&fileName=lvr_landcsv.zip"
 response = requests.get(url)
 with zipfile.ZipFile('lvr_landcsv.zip', mode='r') as zf:
