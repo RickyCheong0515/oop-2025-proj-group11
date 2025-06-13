e44f51e3d262fe02046bf11e63c1f30c31f6bf55
RickyCheong0515 <101554818+RickyCheong0515@users.noreply.github.com>
2025-06-14 02:23:32 +0800

    Update Crawling.py

--- Data_crawling/Crawling.py
+++ Data_crawling/Crawling.py
@@ -2,3 +2,12 @@ Sales_all = pd.DataFrame([])
 Luxury_Sales = pd.DataFrame([])
 Non_Luxury_Sales = pd.DataFrame([])
 Rent_all = pd.DataFrame([])
+
+for year in range(112,114,1):
+  for season in range(1,5,1):
+    url = f"https://plvr.land.moi.gov.tw/DownloadSeason?season={year}S{season}&type=zip&fileName=lvr_landcsv.zip"
+    response = requests.get(url)
+    with zipfile.ZipFile('lvr_landcsv.zip', mode='r') as zf:
+        nameList = zf.namelist()
+        for name in nameList:
+            zf.extract(name, r'zipfolder')
